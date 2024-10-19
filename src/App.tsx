f88dcd3c0ba00823e723fab65b8d1137d5c6c07e
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-19 03:56:16 +0100

    toggle feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,13 @@
+import useLocalStorage from "use-local-storage";
+import "./App.css";
+import Toggle from "./components/Toggle";
 import Sidenav from "./components/Sidenav";
 
 function App() {
+  const [isDark, setIsDark] = useLocalStorage("isDark", false);
   return (
-    <div>
+    <div className="App" data-theme={isDark ? "dark" : "light"}>
+      <Toggle isChecked={isDark} handleChange={() => setIsDark(!isDark)} />
       <Sidenav />
     </div>
   );
--- src/components/Sidenav.tsx
+++ src/components/Sidenav.tsx
@@ -1,47 +1,100 @@
-import { AiOutlineHome, AiOutlineMail, AiOutlineProject } from "react-icons/ai";
-import { BsPerson } from "react-icons/bs";
-import { GrProjects } from "react-icons/gr";
+import { AiOutlineHome } from "react-icons/ai";
+import { CiSettings } from "react-icons/ci";
+import { FaCircleUser } from "react-icons/fa6";
+import { GiSpeaker } from "react-icons/gi";
+import { IoMdNotificationsOutline } from "react-icons/io";
+import { IoChatbubblesOutline } from "react-icons/io5";
+import { MdOutlineEventNote } from "react-icons/md";
+import { TbReport } from "react-icons/tb";
 
 const Sidenav = () => {
   return (
     <div>
-      <div className="md:block hidden fixed top-28">
+      <div className="md:block hidden fixed top-8">
         <div className="flex flex-col">
-          <a
-            href="#main"
-            className="text-black rounded-full shadow-lg bg-gray-300 m-2 p-4 cursor-pointer 
+          <div className="flex items-center">
+            <a
+              href="#main"
+              className="text-[#4169e1] m-0 p-4 cursor-pointer 
           hover:scale-125 ease-in duration-300"
-          >
-            <AiOutlineHome size={20} />
-          </a>
-          <a
-            href="#work"
-            className="text-black rounded-full shadow-lg bg-gray-300 m-2 p-4 cursor-pointer 
+            >
+              <AiOutlineHome size={20} />
+            </a>
+            <h6 className="text-[#4169e1] font-normal">Home</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#work"
+              className="text-black m-0 p-4 cursor-pointer 
           hover:scale-125 ease-in duration-300"
-          >
-            <GrProjects size={20} />
-          </a>
-          <a
-            href="#projects"
-            className="text-black rounded-full shadow-lg bg-gray-300 m-2 p-4 cursor-pointer 
+            >
+              <MdOutlineEventNote size={20} />
+            </a>
+            <h6 className="font-normal">Events</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#projects"
+              className="text-black m-0 p-4 cursor-pointer 
           hover:scale-125 ease-in duration-300"
-          >
-            <AiOutlineProject size={20} />
-          </a>
-          <a
-            href="#main"
-            className="text-black rounded-full shadow-lg bg-gray-300 m-2 p-4 cursor-pointer 
+            >
+              <GiSpeaker size={20} />
+            </a>
+            <h6 className="font-normal">Speaker</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#main"
+              className="text-black m-0 p-4 cursor-pointer 
           hover:scale-125 ease-in duration-300"
-          >
-            <BsPerson size={20} />
-          </a>
-          <a
-            href="#contact"
-            className="text-black rounded-full shadow-lg bg-gray-300 m-2 p-4 cursor-pointer 
+            >
+              <TbReport size={20} />
+            </a>
+            <h6 className="font-normal">Report</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#contact"
+              className="text-black m-0 p-4 cursor-pointer 
           hover:scale-125 ease-in duration-300"
-          >
-            <AiOutlineMail size={20} />
-          </a>
+            >
+              <IoMdNotificationsOutline size={20} />
+            </a>
+            <h6 className="font-normal">Notifications</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#contact"
+              className="text-black m-0 p-4 cursor-pointer 
+          hover:scale-125 ease-in duration-300"
+            >
+              <IoChatbubblesOutline size={20} />
+            </a>
+            <h6 className="font-normal">Messages</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#contact"
+              className="text-black m-0 p-4 cursor-pointer 
+          hover:scale-125 ease-in duration-300"
+            >
+              <CiSettings size={20} />
+            </a>
+            <h6 className="font-normal">Settings</h6>
+          </div>
+          <div className="flex items-center">
+            <a
+              href="#contact"
+              className="text-black m-0 p-4 cursor-pointer 
+          hover:scale-125 ease-in duration-300"
+            >
+              <FaCircleUser size={20} />
+            </a>
+            <div className="inline-block">
+              <h6 className="font-normal">Rudra Devi</h6>
+              <h6 className="font-normal">rudra.devi@gmail.com</h6>
+            </div>
+          </div>
         </div>
       </div>
     </div>
