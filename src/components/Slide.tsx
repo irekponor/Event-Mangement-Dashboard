67e9abf51e52b2f4dc4cb526fd5a2ef692a5bc8b
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-23 00:46:23 +0100

    carousel

--- src/components/Slide.tsx
+++ src/components/Slide.tsx
@@ -1,45 +1,63 @@
 import Carousel from "react-bootstrap/Carousel";
 import Img1 from "../assets/Img1.jfif";
 import img2 from "../assets/img2.jfif";
-import img3 from "../assets/img3.jfif";
+import img from "../assets/img.jfif";
 
 const Slide = () => {
   return (
-    <div>
-      <Carousel>
+    <div className="pl-96">
+      <Carousel className="max-w-[530px] top-32 left-96">
         <Carousel.Item>
-          <img src={Img1} className="object-cover h-screen w-full" alt="" />
+          <img src={Img1} className="object-cover rounded-lg" alt="" />
           <div>
-            <div className="absolute top-0 w-full left-0">
-              <div className="max-w-[450px] w-full h-full md:pl-10 pl-[77px] m-auto justify-center items-center lg:items-start flex-col">
-                <h3 className="font-title1 text-white md:pt-20 pt-10 font-bold md:tracking-[1.6rem] tracking-[1.3rem] mb-4">
-                  Mercury
-                </h3>
+            <div className="absolute top-[220px] w-full">
+              <div className="max-w-[450px] w-full h-full pl-1 justify-center items-center flex-col">
+                <p className="pl-7 text-white font-medium font-title text-[15px] pt-10 mb-4">
+                  Latest News & Updates
+                </p>
               </div>
+              <p className="pl-7 text-white font-light font-title text-[13px]">
+                Turpis interdum nunc varius ornare dignissim pretium. Massa
+                ornare quis aliquet sed vitae. Sed velit nisi, fermentum erat.
+                Fringilla purus, erat fringilla tincidunt quisque non.
+                Pellentesque in ut tellus.
+              </p>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img src={img2} className="object-cover h-screen w-full" alt="" />
+          <img src={img2} className="object-cover rounded-lg" alt="" />
           <div>
-            <div className="absolute top-0 w-full left-0">
-              <div className="max-w-[360px] w-full h-full md:pl-10 pl-24 m-auto justify-center items-center lg:items-start flex-col">
-                <h3 className="font-title1 text-white md:pt-20 pt-10 font-bold tracking-[1.6rem] md:mb-4 mb-[13px]">
-                  Venus
-                </h3>
+            <div className="absolute top-[220px] w-full">
+              <div className="max-w-[450px] w-full h-full pl-1 justify-center items-center flex-col">
+                <p className="pl-7 text-white font-medium font-title text-[15px] pt-10 mb-4">
+                  Latest News & Updates
+                </p>
               </div>
+              <p className="pl-7 text-white font-light font-title text-[13px]">
+                Turpis interdum nunc varius ornare dignissim pretium. Massa
+                ornare quis aliquet sed vitae. Sed velit nisi, fermentum erat.
+                Fringilla purus, erat fringilla tincidunt quisque non.
+                Pellentesque in ut tellus.
+              </p>
             </div>
           </div>
         </Carousel.Item>
         <Carousel.Item>
-          <img src={img3} className="object-cover h-screen w-full" alt="" />
+          <img src={img} className="object-cover rounded-lg" alt="" />
           <div>
-            <div className="absolute top-0 w-full left-0">
-              <div className="max-w-[360px] w-full h-full md:pl-10 pl-24 m-auto justify-center items-center lg:items-start flex-col">
-                <h3 className="font-title1 text-white md:pt-20 pt-10 font-bold tracking-[1.6rem] md:mb-4 mb-[13px]">
-                  Venus
-                </h3>
+            <div className="absolute top-[170px] w-full">
+              <div className="max-w-[450px] w-full h-full pl-1 justify-center items-center flex-col">
+                <p className="pl-7 text-white font-medium font-title text-[15px] pt-10 mb-2">
+                  Latest News & Updates
+                </p>
               </div>
+              <p className="pl-7 text-white font-light font-title text-[13px]">
+                Turpis interdum nunc varius ornare dignissim pretium. Massa
+                ornare quis aliquet sed vitae. Sed velit nisi, fermentum erat.
+                Fringilla purus, erat fringilla tincidunt quisque non.
+                Pellentesque in ut tellus.
+              </p>
             </div>
           </div>
         </Carousel.Item>
