7bad98cb8abccc785c1f28374378a6a5b1925619
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-24 22:43:45 +0100

    tables

--- src/components/History.tsx
+++ src/components/History.tsx
@@ -1,50 +1,26 @@
-import Table from "react-bootstrap/Table";
+import { BsSearch } from "react-icons/bs";
 
 const History = () => {
   return (
     <div>
-      <Table responsive="md">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-            <th>Table heading</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="relative w-full max-w-[200px] top-5 flex">
+        <div>
+          <input
+            className="text-[#eeeeee] outline outline-1 outline-outline-color border-none px-10 py-2 w-52 h-8"
+            type="text"
+            placeholder="Search..."
+          />
+          <BsSearch
+            className="top-0 absolute right-0 mt-[11px] mr-[169px] text-gray-500 font-semibold"
+            size={14}
+          />
+        </div>
+        <div>
+          <button className="m-3 p-2  outline outline-1 outline-outline-color text-black">
+            Date
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
