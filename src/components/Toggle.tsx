c0008e27ea740183925756cde502637b249d273e
emmanuel <emmanuelirekponor86@gmail.com>
2024-10-29 05:06:56 +0100

    responsive view

--- src/components/Toggle.tsx
+++ src/components/Toggle.tsx
@@ -1,6 +1,11 @@
 import "./Toggle.css";
 
-const Toggle = ({ handleChange, isChecked }) => {
+interface ToggleProps {
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  isChecked: boolean;
+}
+
+const Toggle: React.FC<ToggleProps> = ({ handleChange, isChecked }) => {
   return (
     <div>
       <div className="toggle-container flex items-center">
